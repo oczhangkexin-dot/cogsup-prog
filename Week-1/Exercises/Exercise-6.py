27e2311f473682a0c121c13f2f8083628c9cf889
Kexin Zhang <kexinzhang@etu.u-paris.fr>
2026-02-25 14:21:51 +0100

    Add answers to Exercise 6

--- Week-1/Exercises/Exercise-6.py
+++ Week-1/Exercises/Exercise-6.py
@@ -3,13 +3,31 @@ Write a script that lists all the prime numbers between 1 and 10000.
 (A prime number is an integer greater or equal to 2 which has no divisors except 1 and itself). 
 Hint: Write an is_factor helper function.
 """
+import math
 
 def is_factor(d, n):
-    """True iff (if and only if) d is a divisor of n."""
-    pass
+        if d !=0 and n % d == 0:
+            return True
+        else:
+            return False
 
 def is_prime(n):
-    pass
+    d = []
+    for i in range (int(math.sqrt(n))+1):
+        if is_factor(i, n):
+            if i != n and i != 1:
+                return False
+            else:
+                d.append(i)
+    if len(d) == 1:
+         return n
 
-list_of_primes = []
-print(list_of_primes)
+def main():
+    list_of_primes = []
+    for i in range(10001):
+        if is_prime(i):
+            list_of_primes.append(is_prime(i)) 
+    print(list_of_primes)
+
+main()
+
